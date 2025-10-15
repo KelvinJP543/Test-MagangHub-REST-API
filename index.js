73ed1ab0ab254b44cf78d4387218f45b373cc593
Kelvin Jaya Pratama <kevin.pratama301@gmail.com>
2025-10-15 21:41:58 +0700

    Fix for deployment vercel

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.use((req, res) => {
   res.status(404).json({ success: false, error: 'Endpoint not found' });
 });
 
-const PORT = process.env.PORT || 3000;
+//Run for local development
+/*const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
-});
+});*/
+
+module.exports = app;
